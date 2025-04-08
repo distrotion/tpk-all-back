1d7c565f34df6ea3ce7315f8da8ecb7cf3fe3bc6
Thai Parkerizing <administrator@Thais-MacBook-Pro.local>
2025-04-08 08:48:32 +0700

    .

--- flow/001/10GETDATAFROMJOBBINGAQC.js
+++ flow/001/10GETDATAFROMJOBBINGAQC.js
@@ -599,6 +599,13 @@ router.post('/10GETDATAFROMJOBBINGAQC/GENFLODER', async (req, res) => {
 
   let output = {};
 
+  if(input['name']!= undefined){
+    var dir = `C:\\Users\\Administrator\\Desktop\\${input['name']}`;
+    if (!fs.existsSync(dir)){
+      fs.mkdirSync(dir, { recursive: true });
+    }
+  }
+
 
   // let output = {};
 
@@ -615,10 +622,7 @@ router.post('/10GETDATAFROMJOBBINGAQC/GENFLODER', async (req, res) => {
 
   // createFolder("\\172.20.10.150\\sap_s4hana\\S4PRD\\TEST");
   // var dir = '\\\\172.20.10.150\\sap_s4hana\\S4PRD\\TEST';
-  var dir = 'C:\\Users\\Administrator\\Desktop\\test2';
-  if (!fs.existsSync(dir)){
-    fs.mkdirSync(dir, { recursive: true });
-  }
+  
 
 
   //-------------------------------------
